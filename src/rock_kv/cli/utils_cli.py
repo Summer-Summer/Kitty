c5184c97339f0476b241e675b5c6a306d1809d3c
Haojun Xia <xhjustc@mail.ustc.edu.cn>
2025-07-09 07:22:54 -0700

    fix a bug

--- src/rock_kv/cli/utils_cli.py
+++ src/rock_kv/cli/utils_cli.py
@@ -9,5 +9,5 @@ def update_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument("--vbits",              type=int, default=2,        help="Number of bits for V Cache")
     parser.add_argument("--promote_ratio",      type=float, default=0.0,    help="Keep ratio (fp16) for mixed-precision K cache, default=0.0")
     parser.add_argument("--promote_bit",        type=int, default=4,        help="Promote bit for K cache, default=4")
-    parser.add_argument("--channel_selection",  type=int, default=-1,       choices=[-1, 0, 1, 2], help="Channel selection method: 0 for random, 1 for Vanil, 2 for RoPE-aware")
+    parser.add_argument("--channel_selection",  type=int, default=-1,       choices=[-1, 0, 1, 2, 3], help="Channel selection method: 0 for Random, 1 for Variance, 2 for Magnitude, 3 for RoPE-aware")
     return parser
